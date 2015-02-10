f72a7195c7d1ef1592012c958cafdf6750c652aa
Hackbright Student <info@hackbrightacademy.com>
2015-02-10 15:39:18 -0800

    Through Part 2: Chapter 2. Set up relationships betweentables. Etc.

---
+++ judgement.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, request
+import model
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    user_list = model.Session.query(model.User).all()
+    return render_template("user_list.html", users=user_list)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
--- model.py
+++ model.py
@@ -1,11 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import sessionmaker
-ENGINE = None
-Session = None
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+
+
+ENGINE = create_engine("sqlite:///ratings.db", echo=False)
+Session = scoped_session(sessionmaker(bind=ENGINE, autocommit = False, autoflush = False))
 
 Base = declarative_base()
+Base.query = Session.query_property()
 
 ### Class declarations go here
 class User(Base):
@@ -29,15 +35,22 @@ class Movie(Base):
     released_at = Column(DateTime(timezone=False), nullable=True)
     imdb_url = Column(String(200), nullable=True)
 
+
     def __repr__(self):
         return "Movie_id: %d, name: %s, release_date: %r, imdb_url: %s" %(self.id, self.name, self.released_at, self.imdb_url)
 
 class Rating(Base):
     __tablename__ = "rating"
     id = Column(Integer, primary_key = True)
-    movie_id = Column(Integer, nullable = False)
-    user_id = Column(Integer, nullable = False)
+    movie_id = Column(Integer, ForeignKey('movies.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     rating = Column(Integer, nullable = False)
+
+    user = relationship("User",
+        backref=backref("rating", order_by=id))
+
+    movies = relationship("Movie",
+        backref=backref("rating", order_by=id))
     
     def __repr__(self):
         return "Rating id: %d, Movie_id: %d, User_id: %d, Rating: %d" %(self.id, self.movie_id, self.user_id, self.rating)
@@ -46,14 +59,14 @@ class Rating(Base):
 ### End class declarations
 
 
-def connect():
-    global ENGINE
-    global Session
+# def connect():
+#     global ENGINE
+#     global Session
 
-    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
-    Session = sessionmaker(bind=ENGINE)
+#     # ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+#     # Session = sessionmaker(bind=ENGINE)
 
-    return Session()
+#     return Session()
 
 def main():
     """In case we need this for something"""
